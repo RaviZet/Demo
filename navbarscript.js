3aa17b6821ac9b66e723c277e7fd2b43890d45e4
Ravi Joshi <ravi.j@zetwerk.com>
2022-05-22 14:56:52 +0530

    adding for loop with dummy data

--- navbarscript.js
+++ navbarscript.js
@@ -10,3 +10,39 @@ function erase(){
 document.getElementById("myValue").innerHTML = ''
 
 }
+
+// ----------------------------
+
+function setListData(){
+    
+
+    const data = [
+
+        {
+            id : 'id1',
+            name: 'name1'
+        },
+        {
+            id : 'id2',
+            name: 'name2'
+        },
+        {
+            id : 'id3',
+            name: 'name3'
+        }
+
+    ];
+    
+    let myListData = ''
+    for(let i =0 ; i<data.length; i++){
+       myListData = myListData + '<l1>' + data[i].name + '</li> <br>' 
+    }
+    document.querySelector('.navbar #nav-bar-list').innerHTML = myListData;
+}
+
+async function getApiData(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    return data;
+}
